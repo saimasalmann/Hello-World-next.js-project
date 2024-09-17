4529d8bd14dfb142acb2a88b2464207898bd2e58
saimasalmann <786saimasos@gmail.com>
2024-09-17 21:26:39 +0500

    push code

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+   <div>
+    <h1>Hello World !</h1>
+    <h2>hello world</h2>
+   </div>
+     
+      
+  );
+}
