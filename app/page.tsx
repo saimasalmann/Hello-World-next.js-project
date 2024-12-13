9d2ecfcc322b7103db4d7677f116a3c6eccc78f3
saimasalmann <786saimasos@gmail.com>
2024-12-14 00:26:41 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     
-   <div>
-    <h1>Hello World !</h1>
-    <h2>hello world</h2>
-   </div>
+   <>
+    <h1> Hello World </h1>
+   </>
      
       
   );
